b5d789ad4d0862fb6c57876a0af99afb4b19a8bc
Dmitrii Kolbin <dkolbin@gmail.com>
2020-06-04 19:43:17 +0700

    fix tree builder deprecation issue

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,8 +16,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('repregid_api_doc');
+        $treeBuilder = new TreeBuilder('repregid_api_doc');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // symfony < 4.2 support
+            $rootNode = $treeBuilder->root('repregid_api_doc');
+        }
 
         $rootNode
             ->children()
